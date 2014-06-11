aa3744c0aac9efc9ac60d096d9ba5cc8537d8698
Jonathan Pujol <pujol.jonathan@etu.unice.fr>
2014-06-11 10:38:59 +0200

    Instruction avec un switch

--- Simul_Proc-linux-m64/instruction.c
+++ Simul_Proc-linux-m64/instruction.c
@@ -1,3 +1,10 @@
+/*!
+ * \file instruction.c
+ * \brief Description du jeu d'instruction.
+ * \author Jonathan Pujol
+ * \date 11 juin 2014
+ */
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -14,13 +21,23 @@ const char *condition_names[] = {
 	"NC", "EQ", "NE", "GT", "GE", "LT", "LE"
 };
 
+//! Impression de la variable de l'instruction
+/*!
+ *La fonction print_op permet d'imprimer la variable de l'instruction passée en paramètre.
+ *Elle regarde de quel type est l'instruction (direct, indirect, ...) et choisi le mode d'affichage en fonction.
+ *
+ *\param instr l'instruction dont on doit imprimer la variable
+ */
 void print_op(Instruction instr) {
+	// instruction à valeur absolu
 	if (instr.instr_generic._immediate == 0 && instr.instr_generic._indexed == 0) {
 		printf("@0x%.4x", instr.instr_generic._pad);
 	}
+	// instruction immédiate
 	else if(instr.instr_generic._immediate == 1 && instr.instr_generic._indexed == 0) {
 		printf("#%u", instr.instr_generic._pad);
 	}
+	// instruction à une adresse relative indexée
 	else if (instr.instr_generic._immediate == 0 && instr.instr_generic._indexed == 1) {
 		printf("%d[R%.2u]", instr.instr_indexed._offset, instr.instr_indexed._rindex);
 	}
@@ -32,21 +49,38 @@ void print_op(Instruction instr) {
  * \param addr son adresse
  */
 void print_instruction(Instruction instr, unsigned addr) {
-	if (instr.instr_generic._cop == RET || instr.instr_generic._cop == HALT || 
-		   instr.instr_generic._cop == ILLOP || instr.instr_generic._cop == NOP) {
-		printf("%s", cop_names[instr.instr_generic._cop]);
-	}
-	else if (instr.instr_generic._cop == LOAD || instr.instr_generic._cop == STORE || 
-	   instr.instr_generic._cop == ADD || instr.instr_generic._cop == SUB) {
-		printf("%s R%.2d, ", cop_names[instr.instr_generic._cop], instr.instr_generic._regcond);
-		print_op(instr);
-	}
-	else if (instr.instr_generic._cop == PUSH || instr.instr_generic._cop == POP) {
-		printf("%s ", cop_names[instr.instr_generic._cop]);
-		print_op(instr);
-	}
-	else {
-		printf("%s %s, ", cop_names[instr.instr_generic._cop], condition_names[instr.instr_generic._regcond]);
-		print_op(instr);
+	switch ( instr.instr_generic._cop) {
+		case RET :
+		case HALT :
+		case ILLOP :
+		case NOP :
+		{
+			printf("%s", cop_names[instr.instr_generic._cop]);
+		}
+		break;
+
+		case LOAD :
+		case STORE :
+		case ADD :
+		case SUB : 
+		{
+			printf("%s R%.2d, ", cop_names[instr.instr_generic._cop], instr.instr_generic._regcond);
+			print_op(instr);
+		}
+		break;
+
+		case PUSH :
+		case POP :
+		{
+			printf("%s ", cop_names[instr.instr_generic._cop]);
+			print_op(instr);
+		}
+		break;
+
+		default :
+		{
+			printf("%s %s, ", cop_names[instr.instr_generic._cop], condition_names[instr.instr_generic._regcond]);
+			print_op(instr);	
+		}
 	}
 }
