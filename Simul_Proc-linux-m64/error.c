a79fefe51300f2e795313b7b9afbf88e360c733d
danhe <dan_emilie@yahoo.com>
2014-06-11 14:40:05 +0200

    modifier le format des informations

--- Simul_Proc-linux-m64/error.c
+++ Simul_Proc-linux-m64/error.c
@@ -13,29 +13,29 @@
  */
 void error(Error err, unsigned addr){
 	switch(err){
-		case 0:
-			printf("There is no error,%#.4x\n",addr);
+		case 0://!< Pas d'erreur
+			printf("There is no error: %#.4x\n",addr);
 			exit(0);
-		case 1:
-			printf("Instruction inconnue,%#.4x\n",addr);
+		case 1://!< Instruction inconnue
+			printf("ERROR: Instruction inconnue %#.4x\n",addr);
 			exit(1);
-		case 2:
-			printf("Instruction illégale,%#.4x\n",addr);
+		case 2://!< Instruction illégale
+			printf("ERROR: Instruction illégale %#.4x\n",addr);
 			exit(1);
-		case 3:
-			printf("Condition illégale,%#.4x\n",addr);
+		case 3://!< Condition illégale
+			printf("ERROR: Condition illégale %#.4x\n",addr);
 			exit(1);
-		case 4:
-			printf("Valeur immédiate interdite,%#.4x\n",addr);
+		case 4://!< Valeur immédiate interdite
+			printf("ERROR: Valeur immédiate interdite %#.4x\n",addr);
 			exit(1);
-		case 5:
-			printf("Violation de taille du segment de texte,%#.4x\n",addr);
+		case 5://!< Violation de taille du segment de texte
+			printf("ERROR: Violation de taille du segment de texte %#.4x\n",addr);
 			exit(1);
-		case 6:
-			printf("Violation de taille du segment de données,%#.4x\n",addr);
+		case 6://!< Violation de taille du segment de données
+			printf("ERROR: Violation de taille du segment de données %#.4x\n",addr);
 			exit(1);
-		case 7:
-			printf("Violation de taille du segment de pile,%#.4x\n",addr);
+		case 7://!< Violation de taille du segment de pile
+			printf("ERROR: Violation de taille du segment de pile %#.4x\n",addr);
 			exit(1);
 		default:
 			exit(1);
@@ -47,6 +47,6 @@ void error(Error err, unsigned addr){
 *    	addr:	adresse de l'erreur 
 */
 void warning(Warning warn, unsigned addr){
-	if(warn==WARN_HALT)
-		printf("Fin normale du programme (sur HALT): %#.4x\n",addr);
+	if(warn==WARN_HALT)//!< Fin normale du programme (sur HALT)
+		printf("WARNING: HALT reached at address %#.4x\n",addr);
 }
