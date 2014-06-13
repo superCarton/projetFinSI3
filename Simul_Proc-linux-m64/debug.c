e764fd59bac1747fd631840e641f0e6dc1e2deca
danhe <dan_emilie@yahoo.com>
2014-06-13 13:02:30 +0200

    consider la situation où les inputs ne sont pas reconnus

--- Simul_Proc-linux-m64/debug.c
+++ Simul_Proc-linux-m64/debug.c
@@ -22,32 +22,36 @@
  #define NMAX 10
 bool debug_ask(Machine *pmach){
 	char input[NMAX];
-	printf("DEBUG? ");
-	scanf("%s",input);
-	if(strcmp(input,"h")==0){
-		printf("h\thelp\n");
-		printf("c\tcontinue(exit interactive debug mode)\n");
-		printf("s\tstep by step(next instruction)\n");
-		printf("RET\tstep by step(next instruction)\n");
-		printf("r\tprint registers\n");
-		printf("d\tprint data memory\n");
-		printf("t\tprint text(program) memory\n");
-		printf("p\tprint text(program) memory\n");
-		printf("m\tprint registers and data memory\n");	
+	while(1){
+		printf("DEBUG? ");
+		scanf("%s",input);
+		if(strcmp(input,"h")==0){
+			printf("h\thelp\n");
+			printf("c\tcontinue(exit interactive debug mode)\n");
+			printf("s\tstep by step(next instruction)\n");
+			printf("RET\tstep by step(next instruction)\n");
+			printf("r\tprint registers\n");
+			printf("d\tprint data memory\n");
+			printf("t\tprint text(program) memory\n");
+			printf("p\tprint text(program) memory\n");
+			printf("m\tprint registers and data memory\n");	
+		}
+		if(strcmp(input,"c")==0)
+			return false;
+		if(strcmp(input,"r")==0)
+			print_cpu(pmach);
+		if(strcmp(input,"RET")==0 || strcmp(input,"s")==0)
+			return true;
+		if(strcmp(input,"d")==0)
+			print_data(pmach);
+		if(strcmp(input,"t")==0)
+			print_program(pmach);
+		if(strcmp(input,"p")==0)
+			print_program(pmach);
+		if(strcmp(input,"m")==0){
+			print_cpu(pmach);
+			print_data(pmach);
+		}
 	}
-	if(strcmp(input,"c")==0)
-		return false;
-	if(strcmp(input,"r")==0)
-		print_cpu(pmach);
-	if(strcmp(input,"d")==0)
-		print_data(pmach);
-	if(strcmp(input,"t")==0)
-		print_program(pmach);
-	if(strcmp(input,"p")==0)
-		print_program(pmach);
-	if(strcmp(input,"m")==0){
-		print_cpu(pmach);
-		print_data(pmach);
-	}
-	return true;
+		return true;
 }
